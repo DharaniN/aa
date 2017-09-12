ea2fc7f6e72415f27e39c6ecd602ff84882cd8d4
DharaniN <31503566+DharaniN@users.noreply.github.com>
2017-09-12 16:58:07 +0530

    Create large.c

---
+++ large.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+double n11,n22,n33;
+printf("enter three numbers");
+scanf(%f%f%f,&n11,&n22,&n33);
+if(n11>=n22)
+{
+if(n22>=n33) 
+printf("large number,n22);
+else
+printf("large number,n33);
+}
+else
+{
+if(n22>=n33)
+printf("large number",n22);
+else
+printf("large number",n33);
+}
+}
