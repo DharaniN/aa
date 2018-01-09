148ad6619f3b4a0eb961e8dcdb447bf14fe9a0a3
DharaniN <31503566+DharaniN@users.noreply.github.com>
2018-01-09 17:02:33 +0530

    Create prime.c

---
+++ prime.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+int num,i,f=0;
+printf("Enter the value");
+sacnf("%d",&n);
+for(i=2;i<n/2,i++)
+{
+if(n%i==0)
+{
+f=1;break;
+}
+if{
+printf("prime");
+}
+else
+{
+printf("not");
+}
+}
+
