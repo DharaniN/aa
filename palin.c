aa6454c2169ec3b2ccac97291d9808cf8762dda9
DharaniN <31503566+DharaniN@users.noreply.github.com>
2018-01-09 16:52:15 +0530

    Create palin.c

---
+++ palin.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int n,rem,rev=0;
+printf("Enter the value");
+while(n!=0);
+{
+rem=n%10;
+rev=rev*10+rem;
+n=n/10;
+}
+if{
+printf("palindrome");
+}
+else{
+printf("not palindrome");
+}
