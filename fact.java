3b2ee0657f87e9de2799684263cf7edb6e2065f6
DharaniN <31503566+DharaniN@users.noreply.github.com>
2018-01-11 16:49:16 +0530

    Create fact.java

---
+++ fact.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+class demo1()
+{
+
+  int cou, num, fact = 1;
+ Scanner s1=new Scanner(System.in);
+  System.out,println("Enter a number to calculate its factorial\n");
+  n=newInt(); 
+  for (cou = 1; cou< num; cou++)
+  {
+  
+    fact = fact * c;
+System.out.println("Factorial of %d = %d\n", n, fact);
+ 
+  return 0;
+  }
+}
