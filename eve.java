87ba487eeb751dff8ea04b407d75b9bcbcb53e1e
DharaniN <31503566+DharaniN@users.noreply.github.com>
2018-01-11 16:16:00 +0530

    Create eve.java

---
+++ eve.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+class get()
+{
+int a,b,c,e;
+Scanner s1=new Scanner(System.in);
+void demo()
+{
+System.in.out.println("Enter the value");
+a=nextInt();
+b=nextInt();
+if(e/2)
+{
+c=+b;
+e=c/2;
+System.out.println("The even between two intervals value",c);
+}
+}
+}
+class display
+{
+public static void main(String args[])
+{
+get g1=new get();
+g1.demo();
+}
+}
+
