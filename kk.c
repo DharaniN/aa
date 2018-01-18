31d9c562f24c3fb7889bd3841a3c91fb35293e47
DharaniN <31503566+DharaniN@users.noreply.github.com>
2018-01-18 18:29:00 +0530

    Create kk.c

---
+++ kk.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+ void main ()
+ {
+ int a={A,B,C,D,E};
+   int i,j;
+  for ( i = 0; i < 10; i++ )
+  {
+      A[ i ] = i + 100;
+}
+   for (j = 0; j < 10; j++ )
+   {
+      printf("ElemenT", j, A[j] );
+   }
+ 
+   
+}
