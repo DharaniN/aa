ee7b53fab58ea8242c3e80b059da4d1e8b61cdb1
DharaniN <31503566+DharaniN@users.noreply.github.com>
2018-01-21 09:55:36 +0530

    Create rev.c

---
+++ rev.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int a[i],a[j],a,n,rev;
+printf("Enter the three number\n ");
+Scanf("%d",&n);
+if(a[i]>a[j])
+{
+a[i]=a[j];
+a[j]=rev;
+rev=a[i];
+printf("reverse order is:"a[i]);
+}
+}
