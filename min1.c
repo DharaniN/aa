3afd0ba7791bb3c5b88989ddf0877525a6d7073a
DharaniN <31503566+DharaniN@users.noreply.github.com>
2018-01-21 09:37:45 +0530

    Create min1.c

---
+++ min1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int number,minimum;
+printf("Enter the three numer:%d\n");
+scanf("%d",&number);
+while(n==3)
+{
+number=number-1;
+minimum=number;
+prnitf("minimum number of element:",minimum);
+}
+}
