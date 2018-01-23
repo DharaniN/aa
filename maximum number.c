41b7789e70584075165629488490b562fcd3e7f0
DharaniN <31503566+DharaniN@users.noreply.github.com>
2018-01-23 16:37:02 +0530

    Create maximum number.c

---
+++ maximum number.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int m[10],i,max;
+printf("Enter the 10 number\n");
+for(i=0;i<10,i++)
+{
+Scanf("%d",&m[i]);
+}
+max=a[0];
+for(i=0;i<n;i++)
+{
+if(m[i]>max)
+{
+max=m[i];
+}
+}
+printf("maximum number is",max);
+}
