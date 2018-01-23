da164b52c0ab53038cb84672485a22cd7952daf8
DharaniN <31503566+DharaniN@users.noreply.github.com>
2018-01-23 16:51:10 +0530

    Create fibb.c

---
+++ fibb.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,n,a=0,b=1,c;
+printf("Enter the value");
+scanf("%d",&n);
+for(i=1;1<=n,++i)
+{
+printf("%d",a)
+c=a+b;
+a=b;
+b=c;
+}
+printf("fibb series is");
+}
