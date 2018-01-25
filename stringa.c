bed3c17b658978c650bf4cb0429e96e84c2568b8
DharaniN <31503566+DharaniN@users.noreply.github.com>
2018-01-25 16:11:08 +0530

    Create new file

---
+++ stringa.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.c>
+void main()
+{
+char a[100];
+int n,i;
+clrscr();
+printf("Enter the number\n");
+scanf("%d\n"&n);
+printf("Enter the letter\n");
+scanf("%s,&a);
+for(i=0;i<=n,i++
+{
+printf("print the letter 3 times",a);
+}
+getch();
+}
