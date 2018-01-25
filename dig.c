3c63cf13568a056e5896c5a87fd9c72bdce58859
DharaniN <31503566+DharaniN@users.noreply.github.com>
2018-01-25 16:56:49 +0530

    Create dig.c

---
+++ dig.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,i,a;
+printf("Enter three value\n");
+scanf("%d",&n);
+for(i=0;i<=n;i++)
+{
+a[i]=n;
+n=n\10;
+n=a[i];
+printf("The values are",n);
+}
+}
+
