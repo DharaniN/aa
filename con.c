ae5c140544b07ab1079a01952f207bf1c9a1d3e5
DharaniN <31503566+DharaniN@users.noreply.github.com>
2018-01-25 16:28:40 +0530

    Create new file

---
+++ con.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,j;
+char m[100],n[100];
+clrscr();
+printf("Enter the string1:",m);
+scanf("%s",&m);
+printf("Enter the string2:",n);
+scanf("%s",&n);
+for(i=0;m[i]=0;++i)
+{
+for (j=0;n[j]=0;i++,j++)
+{
+m[i]=n[i];
+}
+s[i]=0;
+printf("the result is:"s1);
+}
+getch();
+}
