3a2b73f502dbbb35e2841f8744bb3a9090523017
DharaniN <31503566+DharaniN@users.noreply.github.com>
2018-01-25 16:17:42 +0530

    Create world.c

---
+++ world.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+int a,b;
+char c[100],d[100];
+printf("Enter the sentence\n",c,d);
+scanf("%s",&c,&d);
+scanf("%d",&a,&b);
+if(a>=b)
+{
+printf("print the sentence :",d);
+}
+}
