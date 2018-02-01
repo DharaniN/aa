8a0dc9f71ac89d500b3b77d6dc03e22cd14f6fef
DharaniN <31503566+DharaniN@users.noreply.github.com>
2018-02-01 14:26:47 +0530

    Create power.c

---
+++ power.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n=2;
+    printf("Enter the number:");
+    scanf("%d",&n);
+    if(n/2==0)
+    {
+        printf("Yes");
+    }
+    else
+    {
+        printf("No");
+    }
+    return 0;
+}
